67046f3b07c07f27bfdda895455cc5669c57bfc1
Vignesh Kanna <148306286+vigneshkanna7@users.noreply.github.com>
2025-10-10 10:36:40 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,23 @@
+package core.oops;
+
+public class ABC extends CMDA
+{
+	
+   public void kitchen() {
+	   System.out.println("kitchen finished");
+   }
+
+@Override
+public void room() {
+      System.out.println("room finished ...");	
+}
+
+@Override
+public void hall() {
+ 
+	 System.out.println("hall finished ");
+}
+
+   
+   
+}
